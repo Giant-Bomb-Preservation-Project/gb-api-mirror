57502b70f2dce596d71167d6f4f090aff4f9ae29
Michael Enger <git@michaelenger.com>
2025-04-20 14:22:13 +0100

    Add support for downloading reviews

--- utils/resource.py
+++ utils/resource.py
@@ -1,4 +1,5 @@
 from enum import StrEnum
+import re
 
 from utils import api, logger
 
@@ -6,50 +7,71 @@ from utils import api, logger
 # Which image size to download
 IMAGE_SIZE = "original_url"
 
+# Regex for finding the image source attribute
+IMAGE_SOURCE_RE = r"data-img-src=\"(.*?)\""
 
-def _extract_image_field(items: list, field: str) -> list[str]:
+
+def _extract_images_from_field(items: list, field: str) -> list[str]:
     """Extract out the image field from a list of items."""
     return [item[field][IMAGE_SIZE] for item in items if field in item and item[field]]
+
+
+def _extract_images_from_text_field(items: list, field: str) -> list[str]:
+    """Extract images from a text field from a list of items."""
+    images = []
+    for item in items:
+        if field in item and item[field]:
+            images += re.findall(IMAGE_SOURCE_RE, item[field])
+
+    return images
 
 
 class Resource(StrEnum):
     """A resource that is downloadable from the GB API."""
 
+    REVIEWS = "reviews"
     VIDEO_CATEGORIES = "video_categories"
     VIDEO_SHOWS = "video_shows"
     VIDEOS = "videos"
 
     def download_data(self, api_key: str, delay: int) -> list:
         """Download data for this resource."""
-        if self == Resource.VIDEO_CATEGORIES:
-            return api.get_paged_resource(self.value, api_key, delay)
+        data = []
+
+        if self == Resource.REVIEWS:
+            data = api.get_individualized_resource("review", 1000, api_key, delay)
+        elif self == Resource.VIDEO_CATEGORIES:
+            data = api.get_paged_resource(self.value, api_key, delay)
         elif self == Resource.VIDEO_SHOWS:
-            return api.get_paged_resource(self.value, api_key, delay)
+            data = api.get_paged_resource(self.value, api_key, delay)
         elif self == Resource.VIDEOS:
-            return api.get_paged_resource(self.value, api_key, delay)
+            data = api.get_paged_resource(self.value, api_key, delay)
         else:
             logger.error(f"Unable to download data from resource: {self}")
-            return []
+
+        return data
 
     def extract_images(self, data: list) -> list[str]:
         """Extract out all the images from the given resource."""
         images = []
 
-        if self == Resource.VIDEO_CATEGORIES:
-            images += _extract_image_field(data, "image")
+        if self == Resource.REVIEWS:
+            images = _extract_images_from_text_field(data, "description")
+        elif self == Resource.VIDEO_CATEGORIES:
+            images = _extract_images_from_field(data, "image")
         elif self == Resource.VIDEO_SHOWS:
-            images += _extract_image_field(data, "image")
-            images += _extract_image_field(data, "logo")
+            images = _extract_images_from_field(data, "image")
+            images += _extract_images_from_field(data, "logo")
         elif self == Resource.VIDEOS:
-            images += _extract_image_field(data, "image")
+            images = _extract_images_from_field(data, "image")
             # extract the images from the video shows as well in case they didn't come through the API
             video_shows = [
                 video["video_show"]
                 for video in data
                 if "video_show" in video and video["video_show"]
             ]
-            images += _extract_image_field(video_shows, "logo")
-            images += _extract_image_field(video_shows, "image")
+            images += _extract_images_from_field(video_shows, "logo")
+            images += _extract_images_from_field(video_shows, "image")
         else:
             logger.error(f"Unable to extract images for resource: {self}")
 
